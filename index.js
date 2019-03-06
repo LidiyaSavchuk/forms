e4a546b2616f071bce0909e226a16b0dc7bc52c8
LidiyaSavchuk <savchuk.ekibastuz@gmail.com>
2019-03-06 16:11:54 +0700

    Добавлено сохранение

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ document.getElementById('question-save').addEventListener('click', function (){
 	var title=document.getElementById('question-title').value
 	var question={'title': title}
 	firebase.database().ref('users/test/form/1').set(question,onSaveComplete)
+	
 	document.getElementById('loader').setAttribute('style', 'display:block')
+	
 });
+firebase.database().ref('users/test/form/1').on('value', onLoad)
+function onLoad(snapshot){
+	var question=snapshot.val()
+	document.getElementById('question-title').value=question.title
+	
+}
 function onSaveComplete(err){
 	if(err){
 		document.getElementById('message').innerText='Ошибка при сохранении'
@@ -14,4 +22,3 @@ function onSaveComplete(err){
 	}
 	document.getElementById('loader').setAttribute('style', 'display:none')
 }
-
