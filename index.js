f65e2056f600360ada6e2a23af6d45c1edbcec4f
LidiyaSavchuk <savchuk.ekibastuz@gmail.com>
2019-03-06 17:22:06 +0700

    Добавлена форма ввода информации о себе

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ firebase.database().ref('users/test/form/1').on('value', onLoad)
 function onLoad(snapshot){
 	var question=snapshot.val()
 	document.getElementById('question-title').value=question.title
-	
+
 }
 function onSaveComplete(err){
 	if(err){
@@ -19,6 +19,25 @@ function onSaveComplete(err){
 	}else{
 		document.getElementById('message').innerText='Вопрос сохранен!'
 	
+	}
+	document.getElementById('loader').setAttribute('style', 'display:none')
+}
+document.getElementById('about-save').addEventListener('click', function (){
+	var title1=document.getElementById('about-title').value
+	var about={'title1': title1}
+	firebase.database().ref('users/test/profile/about').set(about,onSave)
+	document.getElementById('loader').setAttribute('style', 'display:block')
+});
+firebase.database().ref('users/test/profile/about').on('value', onLoading)
+function onLoading(snapshot){
+	var about=snapshot.val()
+	document.getElementById('about-title').value=about.title1
+}
+function onSave(err){
+	if(err){
+		document.getElementById('message').innerText='Ошибка при сохранении'
+	}else{
+		document.getElementById('message').innerText='Данные сохранены!'
 	}
 	document.getElementById('loader').setAttribute('style', 'display:none')
 }
